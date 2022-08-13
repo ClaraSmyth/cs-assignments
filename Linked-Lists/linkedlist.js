3227a3b0fdeedf124d38969e28dfff495b109345
Clara Smyth <clarasmyth.work@gmail.com>
2022-08-13 23:58:18 +0100

    Added Node factory and LinkedList skeleton

---
+++ Linked-Lists/linkedlist.js
@@ -0,0 +1,18 @@
+const Node = (value = null, nextNode = null) => {
+    return { value, nextNode };
+};
+
+const LinkedList = () => {
+    // Append
+    // Prepend
+    // Size
+    // Head
+    // Tail
+    // At index
+    // Pop
+    // Contains
+    // Find
+    // To String
+    // Insert At
+    // Remove At
+};
