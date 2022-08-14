b28cf74e71c6862d8bcd10bd924f7c84b43fccad
Clara Smyth <clarasmyth.work@gmail.com>
2022-08-14 23:42:21 +0100

    Added head and append functions the list

--- Linked-Lists/linkedlist.js
+++ Linked-Lists/linkedlist.js
@@ -1,18 +1,41 @@
-const Node = (value = null, nextNode = null) => {
+const node = (value = null, nextNode = null) => {
     return { value, nextNode };
 };
 
-const LinkedList = () => {
-    // Append
-    // Prepend
-    // Size
-    // Head
-    // Tail
-    // At index
-    // Pop
-    // Contains
-    // Find
-    // To String
-    // Insert At
-    // Remove At
+const linkedList = () => {
+    let head = null;
+    let size = 0;
+
+    return {
+        // Function returns the current head of the list
+        head() {
+            return head;
+        },
+
+        // Function adds new data to the list
+        append(data) {
+            head = node(data, head);
+        },
+    };
 };
+
+const list = linkedList();
+const test = list.append('New Data');
+const test2 = list.append('New Data 2');
+
+console.log(list.head());
+
+// Methods to be added the the linked list
+
+// Append - Added
+// Prepend
+// Size
+// Head - Added
+// Tail
+// At index
+// Pop
+// Contains
+// Find
+// To String
+// Insert At
+// Remove At
