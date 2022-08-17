afac133bf3157e563031b07094153565b5c80840
Clara Smyth <clarasmyth.work@gmail.com>
2022-08-17 23:52:15 +0100

    Added removal of duplicates from the array

--- Binary-Search-Trees/bst.js
+++ Binary-Search-Trees/bst.js
@@ -5,3 +5,18 @@ const node = (data = null, left = null, right = null) => {
         right,
     };
 };
+
+const tree = () => {
+    let root = null;
+
+    return {
+        buildTree(arr) {
+            let removeDuplicates = [...new Set(arr)];
+        },
+    };
+};
+
+const test = tree();
+const testArray = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+
+test.buildTree(testArray);
