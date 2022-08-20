bd42e4269dcdf9b31e0e03aed0b16b4b80f01d58
Clara Smyth <clarasmyth.work@gmail.com>
2022-08-20 22:03:46 +0100

    Added buildTree function, returns a balanced tree

--- Binary-Search-Trees/bst.js
+++ Binary-Search-Trees/bst.js
@@ -13,7 +13,26 @@ const tree = () => {
         buildTree(arr) {
             const removeDuplicates = [...new Set(arr)];
             const sortedArr = removeDuplicates.sort((a, b) => a - b);
-            console.log(sortedArr);
+
+            function buildSubtrees(arr) {
+                if (arr.length < 1) return null;
+                const middle = Math.floor(arr.length / 2);
+                const firstHalf = arr.slice(0, middle);
+                const secondHalf = arr.slice(middle + 1);
+
+                let root = node(arr[middle]);
+                root.left = buildSubtrees(firstHalf);
+                root.right = buildSubtrees(secondHalf);
+
+                return root;
+            }
+
+            root = buildSubtrees(sortedArr);
+
+            // console.log('ARR', sortedArr);
+            // console.log('First', firstHalf);
+            // console.log('Second', secondHalf);
+            console.log(root);
         },
     };
 };
